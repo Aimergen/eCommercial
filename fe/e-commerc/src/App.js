b320c32c335604a647b04a763c6bc413139860c2
Aimergen <adkuhidan@gmail.com>
2023-03-23 09:34:42 +0800

    added routes

--- fe/e-commerc/src/App.js
+++ fe/e-commerc/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Login from "./components/Login";
 import NavBarFirst from "./components/NavBarFirst";
-
+import {Route, Routes} from 'react-router-dom';
+ 
 
 function App() {
   return <>
-    <Login />
-    {/* <NavBarFirst /> */}
+  <Routes>
+    <Route path="/" element={<NavBarFirst/>}/>
+    <Route path="/login" element={<Login />} />
+  </Routes>
+  
   </>
 }
 
--- fe/e-commerc/src/components/NavBarFirst.jsx
+++ fe/e-commerc/src/components/NavBarFirst.jsx
@@ -5,6 +5,8 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import Login from './Login';
 
 function NavBarFirst() {
   return (
@@ -19,7 +21,7 @@ function NavBarFirst() {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+            <Link to={'/login'}>Link</Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
